1258b921d2d6f8ebf194acfa770cdbbc843041d7
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-10-16 22:27:52 +0200

    created MemoryBookService with methods

--- src/main/java/pl/bookAPI/service/MemoryBookService.java
+++ src/main/java/pl/bookAPI/service/MemoryBookService.java
@@ -1,4 +1,60 @@
 package pl.bookAPI.service;
 
+import org.springframework.stereotype.Service;
+import pl.bookAPI.book.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
 public class MemoryBookService {
+
+    private List<Book> books;
+
+    public MemoryBookService() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        Objects.requireNonNull(book);
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public Book getBook(Long bookId) {
+        return books.stream()
+                .filter(book -> book.getId().equals(bookId))
+                .findFirst().orElse(null);
+    }
+
+    public boolean editBook(Book editedBook) {
+        boolean anyMatch = books.stream()
+                .anyMatch(book -> book.getId().equals(editedBook.getId()));
+        if (anyMatch) {
+            Optional<Book> thisBook = books.stream()
+                    .filter(book -> book.getId().equals(editedBook.getId()))
+                    .findFirst();
+            thisBook.ifPresent(book -> {
+                book.setIsbn(editedBook.getIsbn());
+                book.setTitle(editedBook.getTitle());
+                book.setAuthor(editedBook.getAuthor());
+                book.setPublisher(editedBook.getPublisher());
+                book.setType(editedBook.getType());
+            });
+        }
+        return anyMatch;
+    }
+
+    public void deleteBook(Long bookId) {
+        books.stream()
+                .filter(book -> book.getId().equals(bookId))
+                .limit(1)
+                .forEach(book -> books.remove(book));
+    }
+
 }
