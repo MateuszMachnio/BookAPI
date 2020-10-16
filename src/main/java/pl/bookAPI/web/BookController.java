f5cb8f97591a3c2d7f162f4ff9f3dd0ca67da496
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-10-16 22:54:47 +0200

    added form for adding a book

--- src/main/java/pl/bookAPI/web/BookController.java
+++ src/main/java/pl/bookAPI/web/BookController.java
@@ -1,8 +1,6 @@
 package pl.bookAPI.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.bookAPI.book.Book;
 import pl.bookAPI.service.MemoryBookService;
 
@@ -23,8 +21,35 @@ public class BookController {
         return memoryBookService.getBooks();
     }
 
+    @PostMapping("")
+    public String addBookToList() {
 
+        return "";
+    }
 
+    @RequestMapping("/addBook")
+    public String addBook() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Add Book</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/books\" method=\"post\">\n" +
+                "    <h3>Adding book to list</h3>\n" +
+                "    <p>\n" +
+                "        <input type=\"number\" min=\"0\" step=\"1\" name=\"isbn\" placeholder=\"ISBN\">\n" +
+                "        <input type=\"text\" name=\"title\" placeholder=\"Book title\">\n" +
+                "        <input type=\"text\" name=\"author\" placeholder=\"Author\">\n" +
+                "        <input type=\"text\" name=\"publisher\" placeholder=\"Publisher\">\n" +
+                "        <input type=\"text\" name=\"type\" placeholder=\"Book type\">\n" +
+                "    </p>\n" +
+                "    <button type=\"submit\">Add book</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>";
+    }
 
 }
 
