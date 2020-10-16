694687907bcae20542bd3826ead8f069882a644f
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-10-16 22:30:05 +0200

    created BookController with displayBooks method

--- src/main/java/pl/bookAPI/web/BookController.java
+++ src/main/java/pl/bookAPI/web/BookController.java
@@ -1,4 +1,30 @@
 package pl.bookAPI.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.bookAPI.book.Book;
+import pl.bookAPI.service.MemoryBookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private MemoryBookService memoryBookService;
+
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
+    }
+
+    @GetMapping("")
+    public List<Book> displayBooks() {
+        return memoryBookService.getBooks();
+    }
+
+
+
+
 }
+
