173f59c7db6ca5750b499d7d92dae35b600bb16d
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-10-16 18:19:36 +0200

    project configuration

---
+++ src/main/java/pl/bookAPI/app/AppConfig.java
@@ -0,0 +1,15 @@
+package pl.bookAPI.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.bookAPI")
+public class AppConfig implements WebMvcConfigurer {
+
+
+}
+
